dfb7a4f2fd09024e50d15ed94ccb877195d261d5
Jayant Kathuria <112076263+jayantkathuria7@users.noreply.github.com>
2024-08-03 17:44:20 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def detect_age_gender(image):
 # Detect faces in the frame
 def detect_faces(frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+    face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
     return faces
 
